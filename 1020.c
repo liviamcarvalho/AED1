6deaaecfb5c98288cf96928a5aa1fac0e418cb6a
Livia de Moura Carvalho <carvalho.livia@unifesp.br>
2024-10-15 14:46:42 -0300

    Create 1020.c

---
+++ 1020.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int idadeemdias, anos = 0, meses = 0, dias = 0, i;
+    scanf("%d", &idadeemdias);
+    anos = idadeemdias / 365;
+    idadeemdias %= 365;
+    meses = idadeemdias / 30;
+    idadeemdias %= 30;
+    dias = idadeemdias;
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses);
+    printf("%d dia(s)\n", dias);
+
+    return 0;
+}
