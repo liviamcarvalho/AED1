10ca0653c7ca2cb022430bb7132ac65d69739464
Livia de Moura Carvalho <carvalho.livia@unifesp.br>
2024-11-12 13:35:05 -0300

    Update 1002.c

--- 1002.c
+++ 1002.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main(){
-
     double raio, area, pi = 3.14159;
     scanf("%lf", &raio);
     area = pi * (raio * raio);
     printf("A=%.4f\n", area);
-
     return 0;
 }
