433dccaade4a78d05c90850b80f4156cbfe5c83a
Livia de Moura Carvalho <carvalho.livia@unifesp.br>
2025-01-28 14:25:43 -0300

    Update 2374.c

--- 2374.c
+++ 2374.c
@@ -3,9 +3,9 @@
 int main(){
     int N, M, dif;
     do scanf("%d", &N);
-    while (N < 1 || N > 40);
+    while(N < 1 || N > 40);
     do scanf("%d", &M);
-    while (M < 1 || M > 40);
+    while(M < 1 || M > 40);
     dif = N - M;
     printf("%d\n", dif);
     return 0;
