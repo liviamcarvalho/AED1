9f252782881c2a742e89c100bc81a0959ed46fa4
Livia de Moura Carvalho <carvalho.livia@unifesp.br>
2025-01-28 14:42:17 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-int const n = 400000;
+const int n = 400000;
 
-void insertionsort(int n, int v[]) {
+void insertionsort(int n, int v[]){
     int i, j, x;
-    for (j = 1; j < n; j++) {
+    for(j = 1; j < n; j++){
         x = v[j];
-        for (i = j - 1; i >= 0 && v[i] > x; i--) {
+        for(i = j - 1; i >= 0 && v[i] > x; i--){
             v[i + 1] = v[i];
         }
         v[i + 1] = x;
@@ -18,18 +18,13 @@ void insertionsort(int n, int v[]) {
 int main(){
     clock_t t;
     int vetor[n];
-
     srand((unsigned)time(NULL));
-
-    for(int k = 0; k < n; k++) {
+    for(int k = 0; k < n; k++){
         vetor[k] = rand() % n;
     }
-
     t = clock();
     insertionsort(n, vetor);
     t = clock() - t;
-
     printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
-
     return 0;
 }
