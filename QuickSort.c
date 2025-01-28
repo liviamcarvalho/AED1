ac8f7df38aa167796c7ff033bec13ae80f26873c
Livia de Moura Carvalho <carvalho.livia@unifesp.br>
2025-01-28 14:44:28 -0300

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-int const n = 400000;
+const int n = 400000;
 
-int separa (int p, int r, int v[]) {
+int separa(int p, int r, int v[]){
     int c, j, k, t;
     c = v[r]; j = p;
-    for (k = p; k < r; k++){
-        if (v[k] <= c) {
+    for(k = p; k < r; k++){
+        if(v[k] <= c){
             t = v[j], v[j] = v[k], v[k] = t;
             j++;
         }
@@ -17,9 +17,9 @@ int separa (int p, int r, int v[]) {
     return j;
 }
 
-void quicksort (int p, int r, int v[]) {
+void quicksort(int p, int r, int v[]){
     int j;
-    if (p < r) {
+    if(p < r){
         j = separa (p, r, v);
         quicksort (p, j - 1, v);
         quicksort (j + 1, r, v);
@@ -29,18 +29,13 @@ void quicksort (int p, int r, int v[]) {
 int main(){
     clock_t t;
     int vetor[n];
-
     srand((unsigned)time(NULL));
-
-    for(int k = 0; k < n; k++) {
+    for(int k = 0; k < n; k++){
         vetor[k] = rand() % n;
     }
-
     t = clock();
     quicksort(0, n-1, vetor);
     t = clock() - t;
-
     printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
-
     return 0;
 }
